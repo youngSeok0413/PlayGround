c9af76eaf262fd4d97f3fbccb5737fbba96e521c
YSCHOI <youngSeok0413@gmail.com>
2023-02-08 10:51:34 +0900

    Exercise finished

--- one-person-shooter/one-person-shooter.cpp
+++ one-person-shooter/one-person-shooter.cpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <Windows.h>
 #include <chrono>
+#include <vector>
+#include <utility>
+#include <algorithm>
 
 using namespace std;
 
@@ -111,10 +114,76 @@ int main() {
 					fDistanceToWall = fDepth;
 				}
 				else {
+					if (map.c_str()[nTestX*nMapWidth + nTestY] == '#') {
+						bHitWall = true;
 
+						vector<pair<float, float>> p;
+
+						for(int tx = 0; tx < 2; tx++)
+							for (int ty = 0; ty < 2; ty++) {
+								float vy = (float)nTestY + ty - fPlayerY;
+								float vx = (float)nTestX + tx - fPlayerX;
+								float d = sqrt(vx*vx+ vy * vy);
+								float dot = (fEyeX * vx/d) + (fEyeY*vy/d);
+								p.push_back(make_pair(d, dot));
+							}
+
+						sort(p.begin(), p.end(), [](const pair<float, float>& left, const pair<float, float>& right) {return left.first < right.first; });
+
+						float fBound = 0.01;
+						if (acos(p.at(0).second) < fBound) bBoundary = true;
+						if (acos(p.at(1).second) < fBound) bBoundary = true;
+						if (acos(p.at(2).second) < fBound) bBoundary = true;
+					}
+				}
+			}
+
+			// Calculate distance to ceiling and floor
+			int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+			int nFloor = nScreenHeight - nCeiling;
+
+			// Shader walls based on distance
+			short nShade = ' ';
+			if (fDistanceToWall <= fDepth / 4.0f)			nShade = 0x2588;	// Very close	
+			else if (fDistanceToWall < fDepth / 3.0f)		nShade = 0x2593;
+			else if (fDistanceToWall < fDepth / 2.0f)		nShade = 0x2592;
+			else if (fDistanceToWall < fDepth)				nShade = 0x2591;
+			else											nShade = ' ';		// Too far away
+
+			if (bBoundary)		nShade = ' '; // Black it out
+
+			for (int y = 0; y < nScreenHeight; y++)
+			{
+				// Each Row
+				if (y <= nCeiling)
+					screen[y * nScreenWidth + x] = ' ';
+				else if (y > nCeiling && y <= nFloor)
+					screen[y * nScreenWidth + x] = nShade;
+				else // Floor
+				{
+					// Shade floor based on distance
+					float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
+					if (b < 0.25)		nShade = '#';
+					else if (b < 0.5)	nShade = 'x';
+					else if (b < 0.75)	nShade = '.';
+					else if (b < 0.9)	nShade = '-';
+					else				nShade = ' ';
+					screen[y * nScreenWidth + x] = nShade;
 				}
 			}
 		}
+
+		// Display Stats
+		swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, A=%3.2f FPS=%3.2f ", fPlayerX, fPlayerY, fPlayerA, 1.0f / fElapsedTime);
+
+		// Display Map
+		for (int nx = 0; nx < nMapWidth; nx++)
+			for (int ny = 0; ny < nMapWidth; ny++)
+			{
+				screen[(ny + 1) * nScreenWidth + nx] = map[ny * nMapWidth + nx];
+			}
+		screen[((int)fPlayerX + 1) * nScreenWidth + (int)fPlayerY] = 'P';
+
 
 		// Display Frame
 		screen[nScreenWidth * nScreenHeight - 1] = '\0';
